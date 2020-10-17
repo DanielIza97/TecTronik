8e98e62a1a7c501e7b4059c67838041dfd3ab5f1
hchiluisa207 <70655129+hchiluisa207@users.noreply.github.com>
2020-10-16 20:35:28 -0500

    Update User.php

--- Pruebas/RantiRantiProyecto-Copi/app/User.php
+++ Pruebas/RantiRantiProyecto-Copi/app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','idcedulacliente','rol',
+        'name', 'email', 'password','idcedulacliente','ro',
     ];
 
     /**
