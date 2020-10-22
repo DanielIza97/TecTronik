b72cd1348ac1f83195ddf8e440bc6eaa64963d37
hchiluisa207 <70655129+hchiluisa207@users.noreply.github.com>
2020-10-22 15:09:52 -0500

    Update register.blade.php

--- RantiRantiProyecto - Segundo Avance/resources/views/auth/register.blade.php
+++ RantiRantiProyecto - Segundo Avance/resources/views/auth/register.blade.php
@@ -38,7 +38,11 @@
                         </div>
 
                         <div class="form-group row">
+<<<<<<< HEAD
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('UserName') }}</label>
+=======
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+>>>>>>> parent of c1c0deb... Update register.blade.php
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
